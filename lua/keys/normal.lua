8ea0e320d3b3ef4fcbf3a145bf203b415d6db918
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-11 00:21:54 +0100

    removing the return func (we dont need it), and adding the telescope keybinds to the normal.lua file (they was in the plugins file before)

--- lua/keys/normal.lua
+++ lua/keys/normal.lua
@@ -1,120 +1,243 @@
-return function()
-	-- HEAD: remove the highlighting from the search mode
-	vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
-	-- HEAD: Telescope
-	vim.keymap.set("n", "<leader>t?", "<CMD>Telescope help_tags<CR>", { desc = "Search Help" })
-	vim.keymap.set("n", "<leader>tm", "<CMD>Telescope<CR>", { desc = "Search Select Telescope" })
-	vim.keymap.set("n", "<leader>tw", "<CMD>Telescope grep_string<CR>", { desc = "Search current Word" })
-	vim.keymap.set("n", "<leader>tg", "<CMD>Telescope live_grep<CR>", { desc = "Search by Grep" })
-	vim.keymap.set("n", "<leader>td", "<CMD>Telescope diagnostics<CR>", { desc = "Search Diagnostics" })
-	vim.keymap.set("n", "<leader>tr", "<CMD>Telescope resume<CR>", { desc = "Search Resume" })
-	vim.keymap.set("n", "<leader>t.", "<CMD>Telescope oldfiles<CR>", { desc = "Search Recent Files" })
-	vim.keymap.set("n", "<leader><leader>", "<CMD>Telescope buffers<CR>", { desc = "Find existing buffers" })
-	vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", { desc = "Search Files" })
-
-	-- HEAD: Diagnostic keymaps
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous Diagnostic message" })
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next Diagnostic message" })
-	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic Error messages" })
-	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
-
-	-- HEAD: Key mappings for cycling through quickfix list
-	vim.keymap.set("n", "[q", ":cnext<CR>", { desc = "Go next on Quick fix list" })
-	vim.keymap.set("n", "]q", ":cprev<CR>", { desc = "Go previous on Quick fix list " })
-
-	-- HEAD: trouble
-	vim.keymap.set("n", "<leader>xx", function()
-		require("trouble").toggle()
-	end, { desc = "Open trouble menu" })
-
-	vim.keymap.set("n", "<leader>xw", function()
-		require("trouble").toggle("workspace_diagnostics")
-	end, { desc = "Trouble workspace diagnostics" })
-
-	vim.keymap.set("n", "<leader>xd", function()
-		require("trouble").toggle("document_diagnostics")
-	end, { desc = "Trouble document diagnostics" })
-
-	vim.keymap.set("n", "<leader>xq", function()
-		require("trouble").toggle("quickfix")
-	end, { desc = "Trouble quickfix" })
-
-	vim.keymap.set("n", "<leader>xl", function()
-		require("trouble").toggle("loclist")
-	end, { desc = "Trouble loclist" })
-
-	vim.keymap.set("n", "<leader>xn", function()
-		require("trouble").next({ skip_groups = true, jump = true })
-	end, { desc = "Trouble next" })
-
-	vim.keymap.set("n", "<leader>xb", function()
-		require("trouble").previous({ skip_groups = true, jump = true })
-	end, { desc = "Trouble previous" })
-
-	vim.keymap.set("n", "gR", function()
-		require("trouble").toggle("lsp_references")
-	end, { desc = "Trouble lsp references" })
-
-	-- HEAD: harpoon key maps
-	-- Set up a key mapping to add a file mark in Harpoon
-	vim.keymap.set("n", "ha", function()
-		require("harpoon.mark").add_file()
-	end, { desc = "Harpoon mark" })
-
-	-- HEAD: Set up a key mapping to navigate to the next mark in Harpoon
-	vim.keymap.set("n", "hn", function()
-		require("harpoon.ui").nav_next()
-	end, { desc = "Harpoon next" })
-
-	-- HEAD: Set up a key mapping to navigate to the previous mark in Harpoon
-	vim.keymap.set("n", "hp", function()
-		require("harpoon.ui").nav_prev()
-	end, { desc = "Harpoon previous" })
-
-	-- HEAD: Set up a key mapping to display Harpoon marks
-	vim.keymap.set("n", "hm", function()
-		require("harpoon.ui").toggle_quick_menu()
-	end, { desc = "Telescope Harpoon marks" })
-
-	-- HEAD: Set up a key mapping to use Telescope to display Harpoon marks
-	vim.keymap.set("n", "<Leader>th", ":Telescope harpoon marks<CR>", { desc = "Telescope Harpoon marks" })
-
-	-- HEAD: redu with sift + u
-	vim.api.nvim_set_keymap("n", "<S-u>", "<C-r>", { noremap = true, silent = true, desc = "Just redu" })
-
-	-- HEAD: buffer prev/next key maps
-	-- prev
-	vim.api.nvim_set_keymap("n", "<leader>bb", ":bp<CR>", { noremap = true, silent = true, desc = "Buffer Previous" })
-	-- next
-	vim.api.nvim_set_keymap("n", "<leader>bn", ":bn<CR>", { noremap = true, silent = true, desc = "Buffer Next" })
-
-	-- HEAD: Keybinds to make split navigation easier.
-	--  Use CTRL+<hjkl> to switch between windows
-	--  See `:help wincmd` for a list of all window commands
-	vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-	vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-	vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-	vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
-	-- HEAD: todo comments keymaps
-	vim.keymap.set("n", "]t", function()
-		require("todo-comments").jump_next()
-	end, { desc = "Next todo comment" })
-
-	vim.keymap.set("n", "[t", function()
-		require("todo-comments").jump_prev()
-	end, { desc = "Previous todo comment" })
-	-- Telescope todo
-	vim.keymap.set("n", "<Leader>tt", ":TodoTelescope<CR>", { desc = "Telescope todo marks" })
-
-	-- vim.keymap.set("n", "]t", function()
-	-- 	require("todo-comments").jump_next({ keywords = { "ERROR", "WARNING" } })
-	-- end, { desc = "Next error/warning todo comment" })
-
-	-- TIP: Disable arrow keys in normal mode
-	-- vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-	-- vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-	-- vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-	-- vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+local key = function(key, commands, desc)
+	vim.keymap.set("n", key, commands, { desc = desc or "" })
 end
+
+------------------------------------------------------------------------------
+-- HEAD: remove the highlighting from the search mode
+key("<Esc>", "<cmd>nohlsearch<CR>")
+
+-- HEAD: Telescope
+local builtin = require("telescope.builtin")
+key("<leader>t?", "<CMD>Telescope help_tags<CR>", "Search Help")
+key("<leader>tm", "<CMD>Telescope<CR>", "Search Select Telescope")
+key("<leader>tw", "<CMD>Telescope grep_string<CR>", "Search current Word")
+key("<leader>tg", "<CMD>Telescope live_grep<CR>", "Search by Grep")
+key("<leader>td", "<CMD>Telescope diagnostics<CR>", "Search Diagnostics")
+key("<leader>tr", "<CMD>Telescope resume<CR>", "Search Resume")
+key("<leader>t.", "<CMD>Telescope oldfiles<CR>", "Search Recent Files")
+key("<leader><leader>", "<CMD>Telescope buffers<CR>", "Find existing buffers")
+key("<leader>ff", "<CMD>Telescope find_files<CR>", "Search Files")
+key("<C-s>", function()
+	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
+end, "Fuzzily search in current buffer")
+key("<leader>s/", function()
+	builtin.live_grep({
+		grep_open_files = true,
+		prompt_title = "Live Grep in Open Files",
+	})
+end, "Search in Opened Files")
+key("<leader>sn", function()
+	builtin.find_files({ cwd = vim.fn.stdpath("config") })
+end, "Search Neovim files")
+
+-- HEAD: Diagnostic keymaps
+key("[d", vim.diagnostic.goto_prev, "Go to previous Diagnostic message")
+key("]d", vim.diagnostic.goto_next, "Go to next Diagnostic message")
+key("<leader>e", vim.diagnostic.open_float, "Show diagnostic Error messages")
+key("<leader>q", vim.diagnostic.setloclist, "Open diagnostic Quickfix list")
+
+-- HEAD: Key mappings for cycling through quickfix list
+key("[q", "<cmd>cnext<CR>", "Go next on Quick fix list")
+key("]q", "<cmd>cprev<CR>", "Go previous on Quick fix list ")
+
+-- HEAD: trouble
+key("<leader>xx", function()
+	require("trouble").toggle()
+end, "Open trouble menu")
+
+key("<leader>xw", function()
+	require("trouble").toggle("workspace_diagnostics")
+end, "Trouble workspace diagnostics")
+
+key("<leader>xd", function()
+	require("trouble").toggle("document_diagnostics")
+end, "Trouble document diagnostics")
+
+key("<leader>xq", function()
+	require("trouble").toggle("quickfix")
+end, "Trouble quickfix")
+
+key("<leader>xl", function()
+	require("trouble").toggle("loclist")
+end, "Trouble loclist")
+
+key("<leader>xn", function()
+	require("trouble").next({ skip_groups = true, jump = true })
+end, "Trouble next")
+
+key("<leader>xb", function()
+	require("trouble").previous({ skip_groups = true, jump = true })
+end, "Trouble previous")
+
+key("gR", function()
+	require("trouble").toggle("lsp_references")
+end, "Trouble lsp references")
+
+-- HEAD: harpoon key maps
+-- Set up a key mapping to add a file mark in Harpoon
+key("ha", function()
+	require("harpoon.mark").add_file()
+end, "Harpoon mark")
+
+-- Set up a key mapping to navigate to the next mark in Harpoon
+key("hn", function()
+	require("harpoon.ui").nav_next()
+end, "Harpoon next")
+
+-- Set up a key mapping to navigate to the previous mark in Harpoon
+key("hp", function()
+	require("harpoon.ui").nav_prev()
+end, "Harpoon previous")
+
+-- Set up a key mapping to display Harpoon marks
+key("hm", function()
+	require("harpoon.ui").toggle_quick_menu()
+end, "Telescope Harpoon marks")
+
+-- Set up a key mapping to use Telescope to display Harpoon marks
+key("<Leader>th", ":Telescope harpoon marks<CR>", "Telescope Harpoon marks")
+
+-- HEAD: redu with sift + u
+key("<S-u>", "<C-r>", "Just redu")
+
+-- HEAD: buffer prev/next key maps
+-- prev
+key("<leader>bb", ":bp<CR>", "Buffer Previous")
+-- next
+key("<leader>bn", ":bn<CR>", "Buffer Next")
+
+-- HEAD: bundo tree
+key("<leader>tu", "<cmd>UndotreeToggle<CR>", "Toggle Undotree")
+
+-- HEAD: Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--  See `:help wincmd` for a list of all window commands
+key("<C-h>", "<C-w><C-h>", "Move focus to the left window")
+key("<C-l>", "<C-w><C-l>", "Move focus to the right window")
+key("<C-j>", "<C-w><C-j>", "Move focus to the lower window")
+key("<C-k>", "<C-w><C-k>", "Move focus to the upper window")
+
+-- HEAD: todo comments keymaps
+key("]t", function()
+	require("todo-comments").jump_next()
+end, "Next todo comment")
+
+key("[t", function()
+	require("todo-comments").jump_prev()
+end, "Previous todo comment")
+
+key("<Leader>tt", ":TodoTelescope<CR>", "Telescope todo marks")
+-- HEAD: remove the highlighting from the search mode
+key("<Esc>", "<cmd>nohlsearch<CR>")
+
+-- HEAD: Telescope
+key("<leader>t?", "<CMD>Telescope help_tags<CR>", "Search Help")
+key("<leader>tm", "<CMD>Telescope<CR>", "Search Select Telescope")
+key("<leader>tw", "<CMD>Telescope grep_string<CR>", "Search current Word")
+key("<leader>tg", "<CMD>Telescope live_grep<CR>", "Search by Grep")
+key("<leader>td", "<CMD>Telescope diagnostics<CR>", "Search Diagnostics")
+key("<leader>tr", "<CMD>Telescope resume<CR>", "Search Resume")
+key("<leader>t.", "<CMD>Telescope oldfiles<CR>", "Search Recent Files")
+key("<leader><leader>", "<CMD>Telescope buffers<CR>", "Find existing buffers")
+key("<leader>ff", "<CMD>Telescope find_files<CR>", "Search Files")
+
+-- HEAD: Diagnostic keymaps
+key("[d", vim.diagnostic.goto_prev, "Go to previous Diagnostic message")
+key("]d", vim.diagnostic.goto_next, "Go to next Diagnostic message")
+key("<leader>e", vim.diagnostic.open_float, "Show diagnostic Error messages")
+key("<leader>q", vim.diagnostic.setloclist, "Open diagnostic Quickfix list")
+
+-- HEAD: Key mappings for cycling through quickfix list
+key("[q", ":cnext<CR>", "Go next on Quick fix list")
+key("]q", ":cprev<CR>", "Go previous on Quick fix list ")
+
+-- HEAD: trouble
+key("<leader>xx", function()
+	require("trouble").toggle()
+end, "Open trouble menu")
+
+key("<leader>xw", function()
+	require("trouble").toggle("workspace_diagnostics")
+end, "Trouble workspace diagnostics")
+
+key("<leader>xd", function()
+	require("trouble").toggle("document_diagnostics")
+end, "Trouble document diagnostics")
+
+key("<leader>xq", function()
+	require("trouble").toggle("quickfix")
+end, "Trouble quickfix")
+
+key("<leader>xl", function()
+	require("trouble").toggle("loclist")
+end, "Trouble loclist")
+
+key("<leader>xn", function()
+	require("trouble").next({ skip_groups = true, jump = true })
+end, "Trouble next")
+
+key("<leader>xb", function()
+	require("trouble").previous({ skip_groups = true, jump = true })
+end, "Trouble previous")
+
+key("gR", function()
+	require("trouble").toggle("lsp_references")
+end, "Trouble lsp references")
+
+-- HEAD: harpoon key maps
+-- Set up a key mapping to add a file mark in Harpoon
+key("ha", function()
+	require("harpoon.mark").add_file()
+end, "Harpoon mark")
+
+-- Set up a key mapping to navigate to the next mark in Harpoon
+key("hn", function()
+	require("harpoon.ui").nav_next()
+end, "Harpoon next")
+
+-- Set up a key mapping to navigate to the previous mark in Harpoon
+key("hp", function()
+	require("harpoon.ui").nav_prev()
+end, "Harpoon previous")
+
+-- Set up a key mapping to display Harpoon marks
+key("hm", function()
+	require("harpoon.ui").toggle_quick_menu()
+end, "Telescope Harpoon marks")
+
+-- Set up a key mapping to use Telescope to display Harpoon marks
+key("<Leader>th", ":Telescope harpoon marks<CR>", "Telescope Harpoon marks")
+
+-- HEAD: redu with sift + u
+key("<S-u>", "<C-r>", "Just redu")
+
+-- HEAD: buffer prev/next key maps
+-- prev
+key("<leader>bb", ":bp<CR>", "Buffer Previous")
+-- next
+key("<leader>bn", ":bn<CR>", "Buffer Next")
+
+-- HEAD: bundo tree
+key("<leader>tu", "<cmd>UndotreeToggle<CR>", "Toggle Undotree")
+
+-- HEAD: Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--  See `:help wincmd` for a list of all window commands
+key("<C-h>", "<C-w><C-h>", "Move focus to the left window")
+key("<C-l>", "<C-w><C-l>", "Move focus to the right window")
+key("<C-j>", "<C-w><C-j>", "Move focus to the lower window")
+key("<C-k>", "<C-w><C-k>", "Move focus to the upper window")
+
+-- HEAD: todo comments keymaps
+key("]t", function()
+	require("todo-comments").jump_next()
+end, "Next todo comment")
+
+key("[t", function()
+	require("todo-comments").jump_prev()
+end, "Previous todo comment")
+
+key("<Leader>tt", "<cmd>TodoTelescope<CR>", "Telescope todo marks")
