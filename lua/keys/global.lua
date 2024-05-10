dab6e3ab2bf75f6351262b71f52725d9e02e71f4
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-11 00:22:48 +0100

    removing the return function from other keys files

--- lua/keys/global.lua
+++ lua/keys/global.lua
@@ -1,7 +1,5 @@
-return function()
-	-- HEAD: intract with system clipboard
-	-- Paste
-	vim.api.nvim_set_keymap("", "<Leader>p", '"+p', { noremap = true, desc = "Paste from system clipboard" })
-	-- Yank
-	vim.api.nvim_set_keymap("", "<Leader>y", '"+y', { noremap = true, desc = "Yank to system clipboard" })
-end
+-- HEAD: intract with system clipboard
+-- Paste
+vim.api.nvim_set_keymap("", "<Leader>p", '"+p', { noremap = true, desc = "Paste from system clipboard" })
+-- Yank
+vim.api.nvim_set_keymap("", "<Leader>y", '"+y', { noremap = true, desc = "Yank to system clipboard" })
--- lua/keys/terminal.lua
+++ lua/keys/terminal.lua
@@ -1,9 +1,7 @@
-return function()
-	-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
-	-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
-	-- is not what someone will guess without a bit more experience.
-	--
-	-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
-	-- or just use <C-\><C-n> to exit terminal mode
-	vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-end
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
