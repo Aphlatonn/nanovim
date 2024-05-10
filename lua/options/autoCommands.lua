ca9abef746490be4760a97e1af17090750696625
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-11 00:24:58 +0100

    adding some options (see the diff)

--- lua/options/autoCommands.lua
+++ lua/options/autoCommands.lua
@@ -1,15 +1,13 @@
-return function()
-	-- [[ Basic Autocommands ]]
-	--  See `:help lua-guide-autocommands`
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 
-	-- Highlight when yanking (copying) text
-	--  Try it with `yap` in normal mode
-	--  See `:help vim.highlight.on_yank()`
-	vim.api.nvim_create_autocmd("TextYankPost", {
-		desc = "Highlight when yanking (copying) text",
-		group = vim.api.nvim_create_augroup("rick-highlight-yank", { clear = true }),
-		callback = function()
-			vim.highlight.on_yank()
-		end,
-	})
-end
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("caveman-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
--- lua/options/options.lua
+++ lua/options/options.lua
@@ -1,76 +1,110 @@
-return function()
-	-- See `:help vim.opt`
-	-- NOTE: You can change these options as you wish!
-	-- For more options, you can see `:help option-list`
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+-- For more options, you can see `:help option-list`
 
-	-- Load the start page edit
-	require("options.start_page").setup()
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
 
-	-- Set to true if you have a Nerd Font installed
-	vim.g.have_nerd_font = true
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
 
-	-- Make line numbers default
-	vim.opt.number = true
-	-- You can also add relative line numbers, to help with jumping.
-	--  Experiment for yourself to see if you like it!
-	vim.opt.relativenumber = true
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "i"
 
-	-- Enable mouse mode, can be useful for resizing splits for example!
-	vim.opt.mouse = "i"
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = true
 
-	-- Don't show the mode, since it's already in the status line
-	vim.opt.showmode = false
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
 
-	-- Sync clipboard between OS and Neovim.
-	--  Remove this option if you want your OS clipboard to remain independent.
-	--  See `:help 'clipboard'`
+-- /// Clip board thinks
+-- uncomment this line if you want to sync your vim clipboard with system clipboard
+-- vim.opt.clipboard = "unnamedplus"
 
-	-- /// Clip board thinks
-	-- uncomment this line if you want to sync your vim clipboard with system clipboard
-	-- vim.opt.clipboard = "unnamedplus"
+-- Enable break indent
+vim.opt.breakindent = true
 
-	-- Enable break indent
-	vim.opt.breakindent = true
+-- Save undo history
+vim.opt.undofile = true
 
-	-- Save undo history
-	vim.opt.undofile = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-	-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-	vim.opt.ignorecase = true
-	vim.opt.smartcase = true
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
 
-	-- Keep signcolumn on by default
-	vim.opt.signcolumn = "yes"
+-- Decrease update time
+vim.opt.updatetime = 250
 
-	-- Decrease update time
-	vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
 
-	-- Decrease mapped sequence wait time
-	-- Displays which-key popup sooner
-	vim.opt.timeoutlen = 300
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
-	-- Configure how new splits should be opened
-	vim.opt.splitright = true
-	vim.opt.splitbelow = true
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-	-- Sets how neovim will display certain whitespace characters in the editor.
-	--  See `:help 'list'`
-	--  and `:help 'listchars'`
-	vim.opt.list = true
-	vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
 
-	-- Preview substitutions live, as you type!
-	vim.opt.inccommand = "split"
+-- Show which line your cursor is on
+vim.opt.cursorline = true
 
-	-- Show which line your cursor is on
-	vim.opt.cursorline = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
 
-	-- Minimal number of screen lines to keep above and below the cursor.
-	vim.opt.scrolloff = 10
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
 
-	-- [[ Basic Keymaps ]]
-	--  See `:help vim.keymap.set()`
+-- set the one status line when opening splits
+vim.opt.laststatus = 3
 
-	-- Set highlight on search, but clear on pressing <Esc> in normal mode
-	vim.opt.hlsearch = true
-end
+--------------------
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+-- Disable swap files
+vim.opt.swapfile = false
+
+-- Disable backups
+vim.opt.backup = false
+
+-- Save undo history
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+vim.opt.undofile = true
+
+-- Enable search highlighting
+vim.opt.incsearch = true
+
+-- Enable 24-bit RGB color in the TUI
+vim.opt.termguicolors = true
+
+-- Enable scroll offset
+vim.opt.scrolloff = 8
+
+-- vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes"
+
+-- Set the update time
+vim.opt.updatetime = 50
+
+-- Set the color column
+vim.opt.colorcolumn = "90"
