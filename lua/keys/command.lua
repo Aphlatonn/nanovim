19791ddd0b88106427a819c7d1e682b19b919770
Marwane Souiri <aphlaton@red-chameleon.com>
2024-08-21 17:20:48 +0100

    Adding some keys to the command mode

--- lua/keys/command.lua
+++ lua/keys/command.lua
@@ -1,5 +1,11 @@
---  i hate typing errors
-vim.keymap.set("c", "W", ":w<CR>", { desc = "Save file with capital W" })
-vim.keymap.set("c", "Q", ":q<CR>", { desc = "quit file with capital Q" })
-vim.keymap.set("c", "WQ", ":q<CR>", { desc = "Save and quit file with capital WQ" })
-vim.keymap.set("c", "Q!", ":q!<CR>", { desc = "Quite file without saving with capital Q!" })
+local key = function(key, commands, desc)
+	vim.keymap.set("c", key, commands, { desc = desc or "" })
+end
+
+------------------------------------------------------------------------------
+
+-- i hate typing errors
+key("W", "<cmd>w<CR>", "Save file with capital W")
+key("Q", "<cmd>q<CR>", "quit file with capital Q")
+key("WQ", "<cmd>wq<CR>", "Save and quit file with capital WQ")
+key("Q!", "<cmd>q!<CR>", "Quite file without saving with capital Q!")
