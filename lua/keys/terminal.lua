56ef69b5784cd4efe53c1d927349c0cb85f22173
Marwane Souiri <aphlaton@red-chameleon.com>
2024-08-21 17:35:59 +0100

    Adding some terminal mod keybinds

--- lua/keys/terminal.lua
+++ lua/keys/terminal.lua
@@ -1,7 +1,13 @@
+local key = function(key, commands, desc)
+	vim.keymap.set("t", key, commands, { desc = desc or "" })
+end
+
+------------------------------------------------------------------------------
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+key("<Esc><Esc>", "<C-\\><C-n>", "Exit terminal mode")
