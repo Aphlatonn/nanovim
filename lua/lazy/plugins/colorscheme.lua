598ae41564496ea54e9da5b4f053bd93a869485e
Marwane Souiri <aphlaton@red-chameleon.com>
2024-09-04 18:41:42 +0100

    Switching to the rose pine color scheme

--- lua/lazy/plugins/colorscheme.lua
+++ lua/lazy/plugins/colorscheme.lua
@@ -1,126 +1,86 @@
---[[ return { -- tokyonight
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
+-- // night fox // --
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
 	config = function()
-		require("tokyonight").setup({
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-			light_style = "day", -- The theme is used when the background is set to light
-			transparent = true, -- Enable this to disable setting the background color
-			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-			styles = {
-				-- Style to be applied to different syntax groups
-				-- Value is any valid attr-list value for `:help nvim_set_hl`
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {},
-				variables = {},
-				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "dark", -- style for floating windows
+		require("rose-pine").setup({
+			variant = "auto", -- auto, main, moon, or dawn
+			dark_variant = "main", -- main, moon, or dawn
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+
+			enable = {
+				terminal = true,
+				legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+				migrations = true, -- Handle deprecated options automatically
 			},
-			sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-			hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-			dim_inactive = false, -- dims inactive windows
-			lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 
-			--- You can override specific color groups to use other groups or a hex color
-			--- function will be called with a ColorScheme table
+			styles = {
+				bold = true,
+				italic = true,
+				transparency = true,
+			},
 
-			--- You can override specific highlights to use other groups or a hex color
-			--- function will be called with a Highlights and ColorScheme table
-			on_highlights = function(hl, c)
-				local prompt = "#2d3149"
-				hl.TelescopeNormal = {
-					bg = c.bg_dark,
-					fg = c.fg_dark,
-				}
-				hl.TelescopeBorder = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-				hl.TelescopePromptNormal = {
-					bg = prompt,
-				}
-				hl.TelescopePromptBorder = {
-					bg = prompt,
-					fg = prompt,
-				}
-				hl.TelescopePromptTitle = {
-					bg = prompt,
-					fg = prompt,
-				}
-				hl.TelescopePreviewTitle = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-				hl.TelescopeResultsTitle = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
+			groups = {
+				border = "muted",
+				link = "iris",
+				panel = "surface",
+
+				error = "love",
+				hint = "iris",
+				info = "foam",
+				note = "pine",
+				todo = "rose",
+				warn = "gold",
+
+				git_add = "foam",
+				git_change = "rose",
+				git_delete = "love",
+				git_dirty = "rose",
+				git_ignore = "muted",
+				git_merge = "iris",
+				git_rename = "pine",
+				git_stage = "iris",
+				git_text = "rose",
+				git_untracked = "subtle",
+
+				h1 = "iris",
+				h2 = "foam",
+				h3 = "rose",
+				h4 = "gold",
+				h5 = "pine",
+				h6 = "foam",
+			},
+
+			palette = {
+				-- Override the builtin palette per variant
+				-- moon = {
+				--     base = '#18191a',
+				--     overlay = '#363738',
+				-- },
+			},
+
+			highlight_groups = {
+				-- Comment = { fg = "foam" },
+				-- VertSplit = { fg = "muted", bg = "muted" },
+			},
+
+			before_highlight = function(group, highlight, palette)
+				-- Disable all undercurls
+				-- if highlight.undercurl then
+				--     highlight.undercurl = false
+				-- end
+				--
+				-- Change palette colour
+				-- if highlight.fg == palette.pine then
+				--     highlight.fg = palette.foam
+				-- end
 			end,
 		})
 
-		vim.cmd("colorscheme tokyonight")
-	end,
-}
---]]
-
--- // night fox // --
-return {
-	"EdenEast/nightfox.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- Default options
-		require("nightfox").setup({
-			options = {
-				-- Compiled file's destination location
-				compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-				compile_file_suffix = "_compiled", -- Compiled file suffix
-				transparent = true, -- Disable setting background
-				terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-				dim_inactive = false, -- Non focused panes set to alternative background
-				module_default = true, -- Default enable value for modules
-				colorblind = {
-					enable = false, -- Enable colorblind support
-					simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-					severity = {
-						protan = 0, -- Severity [0,1] for protan (red)
-						deutan = 0, -- Severity [0,1] for deutan (green)
-						tritan = 0, -- Severity [0,1] for tritan (blue)
-					},
-				},
-				styles = { -- Style to be applied to different syntax groups
-					comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-					conditionals = "NONE",
-					constants = "NONE",
-					functions = "NONE",
-					keywords = "NONE",
-					numbers = "NONE",
-					operators = "NONE",
-					strings = "NONE",
-					types = "NONE",
-					variables = "NONE",
-				},
-				inverse = { -- Inverse highlight for different types
-					match_paren = false,
-					visual = false,
-					search = false,
-				},
-				modules = { -- List of various plugins and additional options
-					-- ...
-				},
-			},
-			palettes = {},
-			specs = {},
-			groups = {},
-		})
-
-		-- setup must be called before loading
-		vim.cmd("colorscheme carbonfox")
+		vim.cmd("colorscheme rose-pine")
+		-- vim.cmd("colorscheme rose-pine-main")
+		-- vim.cmd("colorscheme rose-pine-moon")
+		-- vim.cmd("colorscheme rose-pine-dawn")
 	end,
 }
