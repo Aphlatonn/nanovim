f9af495708857e0237c50e431be52f1d8ae4d9e6
Marwane Souiri <aphlaton@red-chameleon.com>
2024-09-17 17:20:46 +0100

    renaming keys folder to keybinds and editing some keybinds

--- lua/keybinds/normal.lua
+++ lua/keybinds/normal.lua
@@ -9,16 +9,17 @@ key("<Esc>", "<cmd>nohlsearch<CR>")
 
 -- HEAD: Telescope
 local builtin = require("telescope.builtin")
-key("<leader>t?", "<CMD>Telescope help_tags<CR>", "Search Help")
-key("<leader>tm", "<CMD>Telescope<CR>", "Search Select Telescope")
-key("<leader>tw", "<CMD>Telescope grep_string<CR>", "Search current Word")
-key("<leader>tg", "<CMD>Telescope live_grep<CR>", "Search by Grep")
-key("<leader>td", "<CMD>Telescope diagnostics<CR>", "Search Diagnostics")
-key("<leader>tr", "<CMD>Telescope resume<CR>", "Search Resume")
-key("<leader>t.", "<CMD>Telescope oldfiles<CR>", "Search Recent Files")
-key("<leader><leader>", "<CMD>Telescope buffers<CR>", "Find existing buffers")
+key("<leader>f?", "<CMD>Telescope help_tags<CR>", "Search Help")
+key("<leader>fm", "<CMD>Telescope<CR>", "Search Select Telescope")
+key("<leader>fw", "<CMD>Telescope grep_string<CR>", "Search current Word")
+key("<leader>fg", "<CMD>Telescope live_grep<CR>", "Search by Grep")
+key("<leader>fd", "<CMD>Telescope diagnostics<CR>", "Search Diagnostics")
+key("<leader>fr", "<CMD>Telescope resume<CR>", "Search Resume")
+key("<leader>f.", "<CMD>Telescope oldfiles<CR>", "Search Recent Files")
+key("<leader>fb", "<CMD>Telescope buffers<CR>", "Find existing buffers")
 key("<leader>ff", "<CMD>Telescope find_files<CR>", "Search Files")
-key("<C-s>", function()
+key("<Leader>ft", "<cmd>TodoTelescope<CR>", "Telescope todo marks")
+key("<leader>f/", function()
 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 		winblend = 10,
 		previewer = false,
@@ -30,9 +31,6 @@ key("<leader>s/", function()
 		prompt_title = "Live Grep in Open Files",
 	})
 end, "Search in Opened Files")
-key("<leader>sn", function()
-	builtin.find_files({ cwd = vim.fn.stdpath("config") })
-end, "Search Neovim files")
 
 -- HEAD: Diagnostic keymaps
 key("[d", vim.diagnostic.goto_prev, "Go to previous Diagnostic message")
@@ -48,14 +46,12 @@ key("]q", "<cmd>cprev<CR>", "Go previous on Quick fix list ")
 key("<S-u>", "<C-r>", "Just redu")
 
 -- HEAD: buffer prev/next key maps
--- prev
 key("<leader>bb", "<cmd>bp<CR>", "Buffer Previous")
--- next
 key("<leader>bn", "<cmd>bn<CR>", "Buffer Next")
--- Recent
+key("<leader>br", "<cmd>b#<CR>", "Buffer Recent")
 key("t", "<cmd>b#<CR>", "Buffer Recent")
 
--- HEAD: bundo tree
+-- HEAD: undo tree
 key("<leader>tu", "<cmd>UndotreeToggle<CR>", "Toggle Undotree")
 
 -- HEAD: Keybinds to make split navigation easier.
@@ -66,39 +62,6 @@ key("<C-l>", "<C-w><C-l>", "Move focus to the right window")
 key("<C-j>", "<C-w><C-j>", "Move focus to the lower window")
 key("<C-k>", "<C-w><C-k>", "Move focus to the upper window")
 
--- HEAD: trouble
-key("<leader>xx", function()
-	require("trouble").toggle()
-end, "Open trouble menu")
-
-key("<leader>xw", function()
-	require("trouble").toggle("workspace_diagnostics")
-end, "Trouble workspace diagnostics")
-
-key("<leader>xd", function()
-	require("trouble").toggle("document_diagnostics")
-end, "Trouble document diagnostics")
-
-key("<leader>xq", function()
-	require("trouble").toggle("quickfix")
-end, "Trouble quickfix")
-
-key("<leader>xl", function()
-	require("trouble").toggle("loclist")
-end, "Trouble loclist")
-
-key("<leader>xn", function()
-	require("trouble").next({ skip_groups = true, jump = true })
-end, "Trouble next")
-
-key("<leader>xb", function()
-	require("trouble").previous({ skip_groups = true, jump = true })
-end, "Trouble previous")
-
-key("gR", function()
-	require("trouble").toggle("lsp_references")
-end, "Trouble lsp references")
-
 -- HEAD: todo comments keymaps
 key("]t", function()
 	require("todo-comments").jump_next()
@@ -107,5 +70,3 @@ end, "Next todo comment")
 key("[t", function()
 	require("todo-comments").jump_prev()
 end, "Previous todo comment")
-
-key("<Leader>tt", "<cmd>TodoTelescope<CR>", "Telescope todo marks")
---
+++ lua/keybinds/visual.lua
@@ -0,0 +1,11 @@
+local key = function(key, commands, desc)
+	vim.keymap.set("v", key, commands, { desc = desc or "" })
+end
+
+------------------------------------------------------------------------------
+
+-- HEAD: move the selected text up or down
+key("J", ":m '>+1<CR>gv=gv", "Move selected text down")
+key("K", ":m '<2<CR>gv=gv", "Move selected text up")
+key("<S-Up>", ":m '<-2<CR>gv=gv", "Move selected text up")
+key("<S-Down>", ":m '>+1<CR>gv=gv", "Move selected text down")
