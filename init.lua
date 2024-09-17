5792c9c4a4e7d7069b859882173e69bbd65dd0cc
Marwane Souiri <aphlaton@red-chameleon.com>
2024-09-17 17:22:16 +0100

    editing init.lua to work with the changes

--- init.lua
+++ init.lua
@@ -4,28 +4,25 @@
 --                                                      --
 ----------------------------------------------------------
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 --  leader key
 vim.g.mapleader = " "
 -- second leader key
 vim.g.maplocalleader = vim.api.nvim_replace_termcodes("<BS>", false, false, true)
 
--- run lazy (and lazy is running the plugins)
-require("lazy.init")
+-- load the plugins
+require("plugins.init")
 
--- run the options
-require("options.options")
+-- load the options
+require("settings.options")
+-- load Autocommands
+require("settings.autoCommands")
+-- load the status line
+require("settings.statusline")
 
--- run the keys
-require("keys.global")
-require("keys.normal")
-require("keys.insert")
-require("keys.visual")
-require("keys.visual_block")
-require("keys.command")
-require("keys.terminal")
-
--- run Autocommands
-require("options.autoCommands")
+-- load the keybinds
+require("keybinds.global")
+require("keybinds.normal")
+require("keybinds.insert")
+require("keybinds.command")
+require("keybinds.visual")
+require("keybinds.visual_block")
