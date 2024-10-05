f7d447acc8800cd077c4d625d9450ffef8d3bcbf
Marwane Souiri <aphlaton@red-chameleon.com>
2024-10-05 16:29:19 +0100

    Changing the color scheme to kanagawa

--- lua/plugins/plugins/colorscheme.lua
+++ lua/plugins/plugins/colorscheme.lua
@@ -1,86 +1,92 @@
--- // night fox // --
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	-- // night fox // --
+	"rebelot/kanagawa.nvim",
+	name = "kanagawa",
 	config = function()
-		require("rose-pine").setup({
-			variant = "auto", -- auto, main, moon, or dawn
-			dark_variant = "main", -- main, moon, or dawn
-			dim_inactive_windows = false,
-			extend_background_behind_borders = true,
+		-- Default options:
+		require("kanagawa").setup({
+			compile = false, -- enable compiling the colorscheme
+			undercurl = true, -- enable undercurls
+			commentStyle = { italic = true },
+			functionStyle = { bold = true },
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = true, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
 
-			enable = {
-				terminal = true,
-				legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-				migrations = true, -- Handle deprecated options automatically
+			colors = {
+				palette = {
+					-- change all usages of these colors
+					sumiInk0 = "#000000",
+					fujiWhite = "#FFFFFF",
+				},
+				theme = {
+					-- change specific usages for a certain theme, or for all of them
+					wave = {
+						ui = {
+							float = {
+								bg = "none",
+							},
+						},
+					},
+					dragon = {
+						syn = {
+							parameter = "yellow",
+						},
+					},
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
 			},
+			overrides = function(colors)
+				local theme = colors.theme
+				return {
+					-- Assign a static color to strings
+					String = { fg = colors.palette.carpYellow, italic = true },
+					-- theme colors will update dynamically when you change theme!
+					SomePluginHl = { fg = colors.theme.syn.type, bold = true },
 
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = true,
-			},
+					NormalFloat = { bg = "none" },
+					FloatBorder = { bg = "none" },
+					FloatTitle = { bg = "none" },
 
-			groups = {
-				border = "muted",
-				link = "iris",
-				panel = "surface",
+					-- Save an hlgroup with dark background and dimmed foreground
+					-- so that you can use it where your still want darker windows.
+					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-				error = "love",
-				hint = "iris",
-				info = "foam",
-				note = "pine",
-				todo = "rose",
-				warn = "gold",
+					-- Popular plugins that open floats will link to NormalFloat by default;
+					-- set their background accordingly if you wish to keep them dark and borderless
+					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 
-				git_add = "foam",
-				git_change = "rose",
-				git_delete = "love",
-				git_dirty = "rose",
-				git_ignore = "muted",
-				git_merge = "iris",
-				git_rename = "pine",
-				git_stage = "iris",
-				git_text = "rose",
-				git_untracked = "subtle",
+					TelescopeTitle = { fg = theme.ui.special, bold = true },
+					TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+					TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+					TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+					TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+					TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+					TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
-				h1 = "iris",
-				h2 = "foam",
-				h3 = "rose",
-				h4 = "gold",
-				h5 = "pine",
-				h6 = "foam",
-			},
-
-			palette = {
-				-- Override the builtin palette per variant
-				-- moon = {
-				--     base = '#18191a',
-				--     overlay = '#363738',
-				-- },
-			},
-
-			highlight_groups = {
-				-- Comment = { fg = "foam" },
-				-- VertSplit = { fg = "muted", bg = "muted" },
-			},
-
-			before_highlight = function(group, highlight, palette)
-				-- Disable all undercurls
-				-- if highlight.undercurl then
-				--     highlight.undercurl = false
-				-- end
-				--
-				-- Change palette colour
-				-- if highlight.fg == palette.pine then
-				--     highlight.fg = palette.foam
-				-- end
+					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+					PmenuSbar = { bg = theme.ui.bg_m1 },
+					PmenuThumb = { bg = theme.ui.bg_p2 },
+				}
 			end,
+			theme = "wave", -- Load "wave" theme when 'background' option is not set
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+				light = "lotus",
+			},
 		})
 
-		vim.cmd("colorscheme rose-pine")
-		-- vim.cmd("colorscheme rose-pine-main")
-		-- vim.cmd("colorscheme rose-pine-moon")
-		-- vim.cmd("colorscheme rose-pine-dawn")
+		-- setup must be called before loading
+		vim.cmd("colorscheme kanagawa-dragon")
 	end,
 }
